6d0fc8ff25b85422cb0305d8ab0c7e0de800f56f
Batu YURT <asdasd019@hotmail.com>
2022-02-06 03:05:56 +0300

    Add project files.

---
+++ GY_WarSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GY_WarSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
